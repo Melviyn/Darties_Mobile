ab2a09a7861885666418a49783c49690ca73c01d
Melviyn <melvin199629@gmail.com>
2016-12-02 14:31:48 +0100

    Add FamProd et Profil

---
+++ app/src/main/java/com/maximeattoumani/darties_mobile/model/FamProd.java
@@ -0,0 +1,36 @@
+package com.maximeattoumani.darties_mobile.model;
+
+/**
+ * Created by melvi on 02/12/2016.
+ */
+
+public class FamProd {
+    private String ID_FAMILLE_PRODUIT;
+    private String LIB_FAMILLE_PRODUIT;
+
+    public String getDATEMAJ_FAMPROD() {
+        return DATEMAJ_FAMPROD;
+    }
+
+    public void setDATEMAJ_FAMPROD(String DATEMAJ_FAMPROD) {
+        this.DATEMAJ_FAMPROD = DATEMAJ_FAMPROD;
+    }
+
+    public String getLIB_FAMILLE_PRODUIT() {
+        return LIB_FAMILLE_PRODUIT;
+    }
+
+    public void setLIB_FAMILLE_PRODUIT(String LIB_FAMILLE_PRODUIT) {
+        this.LIB_FAMILLE_PRODUIT = LIB_FAMILLE_PRODUIT;
+    }
+
+    public String getID_FAMILLE_PRODUIT() {
+        return ID_FAMILLE_PRODUIT;
+    }
+
+    public void setID_FAMILLE_PRODUIT(String ID_FAMILLE_PRODUIT) {
+        this.ID_FAMILLE_PRODUIT = ID_FAMILLE_PRODUIT;
+    }
+
+    private String DATEMAJ_FAMPROD;
+}
---
+++ app/src/main/java/com/maximeattoumani/darties_mobile/model/Profil.java
@@ -0,0 +1,47 @@
+package com.maximeattoumani.darties_mobile.model;
+
+/**
+ * Created by melvi on 02/12/2016.
+ */
+
+public class Profil {
+
+    private int id_profil;
+    private String lib_profil;
+    private String type_zone;
+    private String id_zone;
+
+    public int getId_profil() {
+        return id_profil;
+    }
+
+    public void setId_profil(int id_profil) {
+        this.id_profil = id_profil;
+    }
+
+    public String getLib_profil() {
+        return lib_profil;
+    }
+
+    public void setLib_profil(String lib_profil) {
+        this.lib_profil = lib_profil;
+    }
+
+    public String getType_zone() {
+        return type_zone;
+    }
+
+    public void setType_zone(String type_zone) {
+        this.type_zone = type_zone;
+    }
+
+    public String getId_zone() {
+        return id_zone;
+    }
+
+    public void setId_zone(String id_zone) {
+        this.id_zone = id_zone;
+    }
+
+
+}
